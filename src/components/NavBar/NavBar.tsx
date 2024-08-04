06584eb795bb16d7615754dc4a6de28fda77d87f
joelkimr <joelcena.47@gmail.com>
2024-08-04 17:43:54 +0300

    ft-cominng-soon

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ import Logo from "../../images/logo/logo.svg";
 import Image from "next/image";
 import NavList from "./NavList";
 import { FiMenu } from "@react-icons/all-files/fi/FiMenu";
-import { IoMdClose } from '@react-icons/all-files/io/IoMdClose';
+import { IoMdClose } from "@react-icons/all-files/io/IoMdClose";
 
 const NavBar = () => {
   const [sticky, setSticky] = useState(false);
@@ -26,20 +26,36 @@ const NavBar = () => {
       <nav
         className={`flex flex-row bg-transparent items-center justify-between md:py-3 py-9 px-12 fixed top-0 left-0 right-0 w-full z-50 ${
           sticky ? "shadow-xl !bg-black" : ""
-        }`}>
-        <Link href='/'>
-          <Image src={Logo} alt='logo_img' className='lg:w-40 md:w-56 w-64' />
+        }`}
+      >
+        <Link href="/">
+          <Image src={Logo} alt="logo_img" className="lg:w-40 md:w-56 w-64" />
         </Link>
-        <div onLoad={ () => { setLoadMenu(false) } } onClick={ () => { setShowMenu(false) } } className={ `${showMenu ? 'absolute md:relative top-52 md:top-0  md:left-0 left-[5rem] text-4xl lg:text-sm md:text-base' : 'hidden md:block'}` }>
+        <div
+          onLoad={() => {
+            setLoadMenu(false);
+          }}
+          onClick={() => {
+            setShowMenu(false);
+          }}
+          className={`${
+            showMenu
+              ? "absolute md:relative top-52 md:top-0  md:left-0 left-[5rem] text-4xl lg:text-sm md:text-base"
+              : "hidden md:block"
+          }`}
+        >
           <NavList />
         </div>
-        <button onClick={ () => setShowMenu(!showMenu) } className="text-white text-6xl md:hidden block">
-          { showMenu ? <IoMdClose /> : <FiMenu /> }
+        <button
+          onClick={() => setShowMenu(!showMenu)}
+          className="text-white text-6xl md:hidden block"
+        >
+          {showMenu ? <IoMdClose /> : <FiMenu />}
         </button>
-        <div className='border-[rgb(255,255,255,0.3)] border-solid border-2  p-2 rounded-md hidden md:block'>
-          <Link href='/' className='flex items-center '>
-            <i className='bg-[#FF0336] text-white text-xl py-2 px-3 rounded-md'></i>
-            <h3 className='text-white md:text-[11px] text-2xl font-[600] uppercase ml-2 mr-2 hover:text-red-400 transition duration-500 '>
+        <div className="border-[rgb(255,255,255,0.3)] border-solid border-2  p-2 rounded-md hidden md:block">
+          <Link href="/Schedule" className="flex items-center ">
+            <i className="bg-[#FF0336] text-white text-xl py-2 px-3 rounded-md"></i>
+            <h3 className="text-white md:text-[11px] text-2xl font-[600] uppercase ml-2 mr-2 hover:text-red-400 transition duration-500 ">
               join class now
             </h3>
           </Link>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -13,36 +13,36 @@ const About = () => {
   return (
     <>
       <section>
-        <div className='login-banner relative justify-center flex min-w-[60rem] xl:w-auto lg:w-[110%] md:w-[155%]'>
-          <h1 className='text-white absolute bottom-8 md:text-[30px] text-[50px] font-bold'>
+        <div className="login-banner relative justify-center flex min-w-[60rem] xl:w-auto lg:w-[110%] md:w-[155%]">
+          <h1 className="text-white absolute bottom-8 md:text-[30px] text-[50px] font-bold">
             About Us
           </h1>
         </div>
-        <div className='relative'>
+        <div className="relative">
           <div>
             <Image
               src={Background}
-              alt='home_image'
-              className='lg:min-w-[70rem] md:min-w-[155%] xl:min-w-full absolute xl:min-h-[120%] lg:min-h-[110%] md:min-h-[107%]  md:block hidden -z-30 -top-32'
+              alt="home_image"
+              className="lg:min-w-[70rem] md:min-w-[155%] xl:min-w-full absolute xl:min-h-[120%] lg:min-h-[110%] md:min-h-[107%]  md:block hidden -z-30 -top-32"
             />
           </div>
-          <div className='w-full flex-row justify-center xl:mx-0 lg:top-96 xl:gap-0 lg:gap-16 md:top-[73rem] top-[12prem] md:items-center mt-32 lg:min-w-[70rem] md:min-w-[155%] xl:min-w-full min-w-[210%] md:block hidden'>
-            <div className='flex flex-col items-center xl:w-3/6 lg:w-3/4 md:min-w-full min-w-[120%] p-6'>
-              <div className=''>
-                <div className='flex justify-center'>
-                  <p className='text-white font-semibold md:text-lg text-4xl md:-mt-0 -mt-2 md:pl-0 pl-44 uppercase z-10'>
+          <div className="w-full flex-row justify-center xl:mx-0 lg:top-96 xl:gap-0 lg:gap-16 md:top-[73rem] top-[12prem] md:items-center mt-32 lg:min-w-[70rem] md:min-w-[155%] xl:min-w-full min-w-[210%] md:block hidden">
+            <div className="flex flex-col items-center xl:w-3/6 lg:w-3/4 md:min-w-full min-w-[120%] p-6">
+              <div className="flex flex-col items-center justify-center">
+                <div className="flex justify-center">
+                  <p className="text-white font-semibold md:text-lg text-4xl md:-mt-0 -mt-2 md:pl-0 pl-44 uppercase z-10">
                     who we are
                   </p>
                   <Image
                     src={TitleBg}
-                    alt='text_bg'
-                    className='md:w-64 w-[35rem] absolute -top-0 md:top-3 md:ml-0 ml-44'
+                    alt="text_bg"
+                    className="md:w-64 w-[35rem] absolute -top-0 md:top-3 md:ml-0 ml-44"
                   />
                 </div>
-                <h2 className='md:text-4xl font-bold md:mb-4 text-6xl md:mt-9 mt-24 text-center md:ml-0 ml-36'>
+                <h2 className="md:text-4xl font-bold md:mb-4 text-6xl md:mt-9 mt-24 text-center md:ml-0 ml-36">
                   We Will Give You Strength and Health
                 </h2>
-                <p className='text-[#646464] font-medium md:text-lg text-[33px] md:mt-0 mt-12 text-center md:ml-0 ml-20'>
+                <p className="text-[#646464] font-medium md:text-lg text-[33px] md:mt-0 mt-12 text-center md:ml-0 ml-20 w-9/12 2xl:w-8/12 ">
                   At Gymate, we are dedicated to helping you achieve the body of
                   your dreams. Our expert trainers and nutritionists will work
                   with you to create a personalized fitness and nutrition plan
@@ -50,55 +50,55 @@ const About = () => {
                 </p>
               </div>
 
-              <div className='bg-black/80 lg:w-44 md:w-64 w-80 text-center lg:text-sm md:text-xl text-3xl text-white font-light md:p-4 p-7 hover:bg-secondary transition duration-300 rounded-sm mt-16 md:ml-0 ml-36'>
-                <Link href='/'>CONTACT US</Link>
+              <div className="bg-black/80 lg:w-44 md:w-64 w-80 text-center lg:text-sm md:text-xl text-3xl text-white font-light md:p-4 p-7 hover:bg-secondary transition duration-300 rounded-sm mt-16 md:ml-0 ml-36">
+                <Link href="/">CONTACT US</Link>
               </div>
             </div>
           </div>
-          <div className='container page-padding py-[5rem] min-w-[60rem] xl:w-auto lg:w-[110%] md:w-[155%] z-30'>
-            <div className='md:mt-[10rem] mt-2'>
-              <div className='md:grid md:grid-cols-2 md:grid-rows-2 w-full flex flex-col space-y-10 md:space-y-0'>
-                <div className='w-full bg-white h-[30rem] flex flex-col justify-center items-center text-center px-[100px]'>
+          <div className="container page-padding py-[5rem] min-w-[60rem] xl:w-auto lg:w-[110%] md:w-[155%] z-30">
+            <div className="md:mt-[10rem] mt-2">
+              <div className="md:grid md:grid-cols-2 md:grid-rows-2 w-full flex flex-col space-y-10 md:space-y-0">
+                <div className="w-full bg-white h-[30rem] flex flex-col justify-center items-center text-center px-[100px]">
                   <Image
                     src={Target}
-                    alt='target_img'
-                    className='md:w-[9rem] w-[7rem]'
+                    alt="target_img"
+                    className="md:w-[9rem] w-[7rem]"
                   />
-                  <h2 className='md:text-[36px] text-[50px] font-bold mt-3'>
+                  <h2 className="md:text-[36px] text-[50px] font-bold mt-3">
                     Our History
                   </h2>
-                  <p className='md:text-[16px] text-[30px] font-medium text-[#646464] mt-5'>
+                  <p className="md:text-[16px] text-[30px] font-medium text-[#646464] mt-5">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Molestias autem tempore facere esse nam in rem vero
                     assumenda aperiam debitis facilis velit, optio quam
                     reiciendis suscipit enim praesentium. Ducimus, cupiditate?
                   </p>
                 </div>
-                <div className='w-full bg-white h-[30rem]'>
+                <div className="w-full bg-white h-[30rem]">
                   <Image
                     src={Img3}
-                    alt='about_img'
-                    className='w-full h-full object-cover bg-center'
+                    alt="about_img"
+                    className="w-full h-full object-cover bg-center"
                   />
                 </div>
-                <div className='w-full bg-white h-[30rem]'>
+                <div className="w-full bg-white h-[30rem]">
                   <Image
                     src={Img4}
-                    alt='about_img'
-                    className='w-full h-full object-cover bg-center'
+                    alt="about_img"
+                    className="w-full h-full object-cover bg-center"
                   />
                 </div>
 
-                <div className='w-full bg-white h-[30rem] flex flex-col justify-center items-center text-center px-[100px]'>
+                <div className="w-full bg-white h-[30rem] flex flex-col justify-center items-center text-center px-[100px]">
                   <Image
                     src={Mountain}
-                    alt='target_img'
-                    className='md:w-[9rem] w-[7rem]'
+                    alt="target_img"
+                    className="md:w-[9rem] w-[7rem]"
                   />
-                  <h2 className='md:text-[36px] text-[50px] font-bold mt-3'>
+                  <h2 className="md:text-[36px] text-[50px] font-bold mt-3">
                     Our Goal
                   </h2>
-                  <p className='md:text-[16px] text-[30px] font-medium text-[#646464] mt-5'>
+                  <p className="md:text-[16px] text-[30px] font-medium text-[#646464] mt-5">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                     Nulla delectus aut enim saepe tempore illo non accusantium?
                     Officia mollitia minus at necessitatibus, nisi ratione!
